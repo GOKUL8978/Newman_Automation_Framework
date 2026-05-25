e3fb9565d6985d236bee2810c88447765cb16262
GOKUL U <111762211+GOKUL8978@users.noreply.github.com>
2026-05-25 09:29:22 +0530

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,3 +1,40 @@
-process.argv[2] = "Create_User";
+// ======================================
+// PROVIDE FOLDER NAME
+// ======================================
+
+// Example:
+// 'Create_User'
+// 'Orders_API'
+// null -> run ROOT
+
+const folderName =
+    'Create_User';
+
+// ======================================
+// PROVIDE ITERATION COUNT
+// ======================================
+
+// Examples:
+//
+// null -> run ALL iterations
+// 1    -> run first iteration only
+// 5    -> run first 5 iterations
+
+const iterationCount =
+    null;
+
+// ======================================
+// SET ARGUMENTS
+// ======================================
+
+process.argv[2] =
+    folderName;
+
+process.argv[3] =
+    iterationCount;
+
+// ======================================
+// RUN FRAMEWORK
+// ======================================
 
 require('./scripts/run-tests');
